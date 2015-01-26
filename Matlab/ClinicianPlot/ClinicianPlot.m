829fa1337c20ba5f5094ce62d10bce6fa2c9010d
Kevin O'Neill <kjon.oneill@gmail.com>
2015-01-26 13:15:05 -0700

    Updated Clinician Plot
    Clinician Plot now includes rudimentary spectrogram plotting. This
    feature needs work to be effective. Particularly, look at the the
    sampling rate when generating fake data.
    
    -Kevin O'Neill

--- Matlab/ClinicianPlot/ClinicianPlot.m
+++ Matlab/ClinicianPlot/ClinicianPlot.m
@@ -22,7 +22,7 @@ function varargout = ClinicianPlot(varargin)
 
 % Edit the above text to modify the response to help ClinicianPlot
 
-% Last Modified by GUIDE v2.5 20-Jan-2015 13:53:02
+% Last Modified by GUIDE v2.5 26-Jan-2015 12:29:51
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -49,18 +49,19 @@ function ClinicianPlot_OpeningFcn(hObject, eventdata, handles, varargin)
 % Choose default command line output for ClinicianPlot
 handles.output = hObject;
 
-handles.fileName = '';
-handles.pathName = '';
-handles.fullName = '';
+handles.fileName = '';  % File Name of chosen data file
+handles.pathName = '';  % Path of chosen data file
+handles.fullName = '';  % The full path of the data file
 
-handles.chans = [1:96];                   % Channels to plot
-handles.numChans = length(handles.chans); % Number of channels
-handles.plotChans = [1:96];
-handles.numPlotChans = length(handles.plotChans);
+handles.chans = [1:96];                           % Chanels of data
+handles.numChans = length(handles.chans);         % Number of channels
+handles.plotChans = [1:96];                       % The channels to plot
+handles.numPlotChans = length(handles.plotChans); % The number of channels to plot
 
 handles.numPatch = 4;               % Number of patches
 handles.scale = 1;                  % Scale: 1 = 1uV, 2 = 10uV, 3=100uV, 4 = 1mV
 handles.data = [];                  % Data to plot
+handles.plotData = [];              % Offset data
 handles.pHandle = [];               % Plot Handle
 handles.time = [];                  % Time points
 handles.timeVals = [0,0];           % Entered time values
@@ -68,10 +69,13 @@ handles.timeBoundCount = [36000];   % Default timeBound in samples. (1 min * 500
 handles.timeBoundVals = [1];        % Time bound on each side of the point of interest. [Min, Sec]. Default = 1min
 handles.specData = [];              % Spectrogram
 handles.plotMode = 1;               % Default Mode is mono-phasic plot
-handles.fs = 500;                   % 500 samples/sec
+handles.fs = 500;                   % 500 samples/sec (Default)
 
 handles.zoomLevel = 1; % 1 minute either side of center
 handles.panLevel = 0; % Center of plot in minutes
+
+handles.specChan = 1;
+handles.specData = [];
 
 % Update handles structure
 guidata(hObject, handles);
@@ -82,177 +86,176 @@ guidata(hObject, handles);
 
 % --- Outputs from this function are returned to the command line.
 function varargout = ClinicianPlot_OutputFcn(hObject, eventdata, handles) 
-
-% Get default command line output from handles structure
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in MonoPhasicButton.
+%%%% MonoPhasicButton %%%%
 function MonoPhasicButton_Callback(hObject, eventdata, handles)
 
+% If the plot mode is not MonoPhasic, switch to MonoPhasic
 if handles.plotMode ~= 1
     handles.plotMode = 1;
     set(handles.StateSTOut, 'String', 'MonoPhasic');
 end % END IF
-guidata(hObject, handles);
+guidata(hObject, handles); % Save handles object
+% END FUNCTION
 
-% --- Executes on button press in BiPhasicButton.
+%%%% BiPhasicButton %%%%
 function BiPhasicButton_Callback(hObject, eventdata, handles)
 
+% If the plot mode is not BiPhasic, switch to BiPhasic
 if handles.plotMode ~= 2
     handles.plotMode = 2;
     set(handles.StateSTOut, 'String', 'BiPhasic');
 end % END IF
-guidata(hObject, handles);
+guidata(hObject, handles);% Save handles object
+% END FUNCTION
 
+%%%% ChannelsEditText %%%%
 function ChannelsET_Callback(hObject, eventdata, handles)
-% Hints: get(hObject,'String') returns contents of ChannelsET as text
-%        str2double(get(hObject,'String')) returns contents of ChannelsET as a double
 
-tmpChan = get(hObject, 'String');
+tmpChan = get(hObject, 'String'); % Gether data
 tmpChan = strrep(tmpChan, ' ', ''); % Removes spaces
 
-chanList = [];
+chanList = []; % Allocate variable
 
+% If there is a dash, parse pairs
 if ~isempty(regexp(tmpChan, '([0-9]+)-([0-9]+)', 'Tokens'))
-    tmpPair = regexp(tmpChan, '([0-9]+)-([0-9]+)', 'Tokens');
+    tmpPair = regexp(tmpChan, '([0-9]+)-([0-9]+)', 'Tokens'); % Extract pairs
     
+    % For each pair, parse pair
     for i = 1:length(tmpPair)
         chanList = [chanList, str2double(tmpPair{i}{1}):str2double(tmpPair{i}{2})];
-    end
-end
+    end % END FOR
+end % END IF
 
+% If there are numbers, parse numbers
 if ~isempty(regexp(tmpChan, '([0-9]+)', 'Tokens'))
-        tmpNum = regexp(tmpChan, '([0-9]+)', 'Tokens');
+    tmpNum = regexp(tmpChan, '([0-9]+)', 'Tokens'); % Extract numbers
     
+    % For each number, parse number
     for i = 1:length(tmpNum)
         chanList = [chanList, str2double(tmpNum{i}{1})];
-    end
-end
+    end % END FOR
+end % END IF
 
+% Select the unique numbers and sort them
 chanList = unique(sort(chanList, 'ascend'));
 
-handles.chans = chanList;
-handles.numChans = length(chanList);
+handles.chans = chanList; % Set selected channels to handles.chans
+handles.numChans = length(chanList); % Find how many channels there are
 
-guidata(hObject, handles);
+guidata(hObject, handles); % Save handles object
+% END FUNCTION
 
 
-% --- Executes during object creation, after setting all properties.
+% ChannelsEditText Creation %
 function ChannelsET_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
-end
+end % END IF
+% END FUNCTION
 
 
-
+%%%% PatchEditText %%%%
 function PatchET_Callback(hObject, eventdata, handles)
-% Hints: get(hObject,'String') returns contents of PatchET as text
-%        str2double(get(hObject,'String')) returns contents of PatchET as a double
 
-tmpPatch = get(hObject,'String');
+tmpPatch = get(hObject,'String'); % Get string
 
-handles.numPatch = str2double(tmpPatch);
-guidata(hObject, handles);
+handles.numPatch = str2double(tmpPatch); % Convert string to double
+guidata(hObject, handles); % Save handles object
+% END FUNCTION
 
-
-
-
-% --- Executes during object creation, after setting all properties.
+% PatchEditText Creation %
 function PatchET_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
-end
+end % END IF
+% END FUCNTION
 
-
-% --- Executes on selection change in ScaleDD.
+%%%% ScaleDropDown %%%%
 function ScaleDD_Callback(hObject, eventdata, handles)
-% Hints: contents = cellstr(get(hObject,'String')) returns ScaleDD contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from ScaleDD
 
-handles.scale = get(hObject,'Value');
-guidata(hObject, handles);
+handles.scale = get(hObject,'Value'); % Get the value of the menu (1 through n)
+guidata(hObject, handles); % Save handles object
+% END FUNCTION
 
-% --- Executes during object creation, after setting all properties.
+% ScaleDropDown Creation %
 function ScaleDD_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
-end
+end % END IF
+% END FUCNTION
 
-
+%%%% TimeEditText %%%%
 function TimeET_Callback(hObject, eventdata, handles)
-% Hints: get(hObject,'String') returns contents of TimeET as text
-%        str2double(get(hObject,'String')) returns contents of TimeET as a double
-
 % ************************************
 % Call a SetTime() function in order to calculate the sameples required.
 % The sampling rate is loaded from the chosen file.
 % ************************************
 
-tmpText = get(hObject,'String');
-regText =  regexp(tmpText, '([0-9]+)', 'Tokens');
+tmpText = get(hObject,'String'); % Gather string
+regText =  regexp(tmpText, '([0-9]+)', 'Tokens'); % Parse numbers in string
 
+% Save the minute time
 handles.timeCount = str2double(regText{1}{1})*60*handles.fs; % Samples per minute;
-handles.timeVals = [str2double(regText{1}{1})];
-set(hObject, 'String', [regText{1}{1}, ':00']);
+handles.timeVals = [str2double(regText{1}{1})]; % Store the minute 
+set(hObject, 'String', [regText{1}{1}, ':00']); % Display time in clock format
 
 % If the user inputs minutes and seconds
 if length(regText) == 2
     handles.timeCount = handles.timeCount + str2double(regText{2}{1})*handles.fs; % Samples per second
-    set(hObject, 'String', [regText{1}{1}, ':', regText{2}{1}]);
-    handles.timeVals(2) = [str2double(regText{2}{1})];
+    set(hObject, 'String', [regText{1}{1}, ':', regText{2}{1}]); % Display time in clock format
+    handles.timeVals(2) = [str2double(regText{2}{1})]; % Store the second
 else
-    handles.timeVals(2) = [0];
+    handles.timeVals(2) = [0]; % If no second was entered, set second to be 0
 end % END IF
 
+% Set the TimeSlideBar default position
 handles.panLevel = handles.timeCount/60/handles.fs;
 
-handles.reLoad = 1;
+handles.reLoad = 1; % Force the data to reload
 
-guidata(hObject, handles);
+guidata(hObject, handles); % Save handles obect
+% END FUNCTION
 
-% END
-
-% --- Executes during object creation, after setting all properties.
+% TimeEditText Creation %
 function TimeET_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
-end
+end % END IF
+% END FUNCTION
 
-
-
+%%%% TimeBoundEditText %%%%
 function TimeBoundET_Callback(hObject, eventdata, handles)
-% hObject    handle to TimeBoundET (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
 % ************************************
 % Call a SetTime() function in order to calculate the sameples required.
 % The sampling rate is loaded from the chosen file.
 % ************************************
 
-tmpText = get(hObject,'String');
-regText =  regexp(tmpText, '([0-9]+)', 'Tokens');
+tmpText = get(hObject,'String'); % Gather string
+regText =  regexp(tmpText, '([0-9]+)', 'Tokens'); % Parse numbers in string
 
+% Save the minute time
 handles.timeBoundCount = str2double(regText{1}{1})*60*handles.fs; % Samples per minute;
-handles.timeBoundVals = [str2double(regText{1}{1})];
-set(hObject, 'String', [regText{1}{1}, ':00']);
+handles.timeBoundVals = [str2double(regText{1}{1})]; % Store the minute 
+set(hObject, 'String', [regText{1}{1}, ':00']);  % Display time in clock format
 
 % If the user inputs minutes and seconds
 if length(regText) == 2
     handles.timeBoundCount = handles.timeBoundCount + str2double(regText{2}{1})*handles.fs; % Samples per second
-    set(hObject, 'String', [regText{1}{1}, ':', regText{2}{1}]);
-    handles.timeBoundVals(2) = [str2double(regText{2}{1})];
+    set(hObject, 'String', [regText{1}{1}, ':', regText{2}{1}]);  % Display time in clock format
+    handles.timeBoundVals(2) = [str2double(regText{2}{1})]; % Store the second 
 else
-    handles.timeBoundVals(2) = [0];
+    handles.timeBoundVals(2) = [0]; % If no second was entered, set second to be 0
 end % END IF
 
+handles.reLoad = 1; % Force the data to reload
 
-handles.reLoad = 1;
+guidata(hObject, handles); % Save the handles object
+% END FUNCTION
 
-guidata(hObject, handles);
-
-% --- Executes during object creation, after setting all properties.
+% TimeBoundEditText Creation %
 function TimeBoundET_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
@@ -318,19 +321,35 @@ handles.fullNime = fullfile(pathName, fileName);
 handles.reLoad = 1;
 
 guidata(hObject, handles);
-% END
+% END FUNCTION
 
 % --- Executes on button press in UpdatePlotButton.
 function UpdatePlotButton_Callback(hObject, eventdata, handles)
-% hObject    handle to UpdatePlotButton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-UpdatePlot(hObject,handles);
+handles = UpdatePlot(hObject,handles);
 guidata(hObject, handles);
-% END
+% END FUNCTION
 
-function UpdatePlot(hObject,handles)
+% --- Executes on button press in MakeSpecPB.
+function MakeSpecPB_Callback(hObject, eventdata, handles)
+handles = MakeSpectrogram(handles);
+guidata(hObject, handles);
+% END FUNCTION
+
+
+
+function SpecChannelET_Callback(hObject, eventdata, handles)
+handles.specChan = str2double(get(hObject, 'String'));
+guidata(hObject, handles);
+% END FUNCTION
+
+% --- Executes during object creation, after setting all properties.
+function SpecChannelET_CreateFcn(hObject, eventdata, handles)
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end % END IF
+% END FUNCTION
+
+function handles = UpdatePlot(hObject,handles)
 
 if handles.reLoad
     handles = ChanSelect(handles);
@@ -478,4 +497,54 @@ end % END IF
 
 % END FUNCTION
 
+
+function handles = MakeSpectrogram(handles)
+
+timeSpan = (handles.timeBoundVals(1)*60+handles.timeBoundVals(2))*2;
+
+handles.specWindow = [2, 0.5];
+handles.specparams.tapers = [1,1];
+handles.specParams.Fs = 1000;
+handles.specParams.Fpass = [0,500];
+handles.specParams.pad = 2;
+
+[handles.specS,handles.specT,handles.specF]=mtspecgramc(handles.data(handles.specChan,:),handles.specWindow,handles.specParams);
+
+figure(2)
+imagesc(handles.specT,handles.specF,10*log10(handles.specS)'); axis xy;
+
+% set(gca, 'YTick', [handles.timeVals(1)-handles.timeBoundVals(1):]
+cbar_axes = colorbar('peer', gca);
+set(get(cbar_axes, 'ylabel'), 'String', 'Power, dB');
+title(sprintf('Channel %d', handles.specChan));
+xlabel('Time, sec')
+ylabel('Frequency, Hz')
+
+guidata(hObject, handles);
+% END FUNCTION
+
+
 % EOF
+
+
+
+function edit8_Callback(hObject, eventdata, handles)
+% hObject    handle to edit8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit8 as text
+%        str2double(get(hObject,'String')) returns contents of edit8 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit8_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end

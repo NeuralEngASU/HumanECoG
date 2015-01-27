cc22c738e3806280654cc21e3507a075a17a15ad
Kevin O'Neill <kjon.oneill@gmail.com>
2015-01-27 15:41:04 -0700

    Updated Clinician Plot
    Fixed case boundary errors and completed the spectrogram code. Code is
    not almost finalized. The next step is to include real LoadData()
    function as well as writing a downsampling code (I have one, but it
    needs some updating).
    
    -Kevin O'Neill

--- Matlab/ClinicianPlot/ClinicianPlot.m
+++ Matlab/ClinicianPlot/ClinicianPlot.m
@@ -1,28 +1,10 @@
 function varargout = ClinicianPlot(varargin)
 % CLINICIANPLOT MATLAB code for ClinicianPlot.fig
-%      CLINICIANPLOT, by itself, creates a new CLINICIANPLOT or raises the existing
-%      singleton*.
-%
-%      H = CLINICIANPLOT returns the handle to a new CLINICIANPLOT or the handle to
-%      the existing singleton*.
-%
-%      CLINICIANPLOT('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in CLINICIANPLOT.M with the given input arguments.
-%
-%      CLINICIANPLOT('Property','Value',...) creates a new CLINICIANPLOT or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before ClinicianPlot_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to ClinicianPlot_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
 % Edit the above text to modify the response to help ClinicianPlot
 
-% Last Modified by GUIDE v2.5 26-Jan-2015 12:29:51
+% Last Modified by GUIDE v2.5 27-Jan-2015 15:36:40
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -88,22 +70,22 @@ varargout{1} = handles.output;
 
 %%%% MonoPhasicButton %%%%
 function MonoPhasicButton_Callback(hObject, eventdata, handles)
-
 % If the plot mode is not MonoPhasic, switch to MonoPhasic
 if handles.plotMode ~= 1
     handles.plotMode = 1;
     set(handles.StateSTOut, 'String', 'MonoPhasic');
+    handles.reLoad = 1;
 end % END IF
 guidata(hObject, handles); % Save handles object
 % END FUNCTION
 
 %%%% BiPhasicButton %%%%
 function BiPhasicButton_Callback(hObject, eventdata, handles)
-
 % If the plot mode is not BiPhasic, switch to BiPhasic
 if handles.plotMode ~= 2
     handles.plotMode = 2;
     set(handles.StateSTOut, 'String', 'BiPhasic');
+    handles.reLoad = 1;
 end % END IF
 guidata(hObject, handles);% Save handles object
 % END FUNCTION
@@ -130,7 +112,7 @@ end % END IF
 if ~isempty(regexp(tmpChan, '([0-9]+)', 'Tokens'))
     tmpNum = regexp(tmpChan, '([0-9]+)', 'Tokens'); % Extract numbers
     
-    % For each number, parse number
+    % For each number, parse
     for i = 1:length(tmpNum)
         chanList = [chanList, str2double(tmpNum{i}{1})];
     end % END FOR
@@ -141,10 +123,10 @@ chanList = unique(sort(chanList, 'ascend'));
 
 handles.chans = chanList; % Set selected channels to handles.chans
 handles.numChans = length(chanList); % Find how many channels there are
+handles.reLoad = 1;
 
 guidata(hObject, handles); % Save handles object
 % END FUNCTION
-
 
 % ChannelsEditText Creation %
 function ChannelsET_CreateFcn(hObject, eventdata, handles)
@@ -152,7 +134,6 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end % END IF
 % END FUNCTION
-
 
 %%%% PatchEditText %%%%
 function PatchET_Callback(hObject, eventdata, handles)
@@ -174,6 +155,7 @@ end % END IF
 function ScaleDD_Callback(hObject, eventdata, handles)
 
 handles.scale = get(hObject,'Value'); % Get the value of the menu (1 through n)
+handles.reLoad = 1;
 guidata(hObject, handles); % Save handles object
 % END FUNCTION
 
@@ -186,11 +168,6 @@ end % END IF
 
 %%%% TimeEditText %%%%
 function TimeET_Callback(hObject, eventdata, handles)
-% ************************************
-% Call a SetTime() function in order to calculate the sameples required.
-% The sampling rate is loaded from the chosen file.
-% ************************************
-
 tmpText = get(hObject,'String'); % Gather string
 regText =  regexp(tmpText, '([0-9]+)', 'Tokens'); % Parse numbers in string
 
@@ -309,6 +286,8 @@ handles.fileName = fileName;
 handles.pathName = pathName;
 handles.fullName = fullfile(pathName, fileName);
 
+set(handles.FileButton, 'String', handles.fileName);
+
 handles.reLoad = 1;
 
 guidata(hObject, handles);
@@ -362,28 +341,48 @@ handles.time = [handles.timeCount - handles.timeBoundCount : handles.timeCount +
 handles.time = handles.time./(60*handles.fs);
 
 % Plot pathces
-elecPerPatch = floor(numPlot/handles.numPatch);
-if mod(elecPerPatch,2) == 0
-    YY = zeros(elecPerPatch, 4);
+if handles.numPatch > 0
     
-    for i = 1:handles.numPatch
-        YY(i,1:4) = [elecPerPatch*(i-1)*10+5 elecPerPatch*(i-1)*10+5, elecPerPatch*i*10+5, elecPerPatch*i*10+5];
-        XX(i,1:4) = [0, 1000, 1000, 0];
+    % Calculate the number of electrodes contained in each patch
+    elecPerPatch = floor(numPlot/handles.numPatch); 
+   
+    YY = zeros(elecPerPatch, 4); % Allocate variable 
+    
+    % For each patch (plus one extra) generate limits of patch
+    for i = 1:handles.numPatch + 1
+        YY(i,1:4) = [elecPerPatch*(i-1)*10+5,...  % Min Y
+                     elecPerPatch*(i-1)*10+5,...  % Min Y
+                     elecPerPatch*i*10+5,...      % Max Y
+                     elecPerPatch*i*10+5];        % Max Y
+                 
+        XX(i,1:4) = [handles.timeVals(1)-handles.timeBoundVals(1)-2,... % Min X
+                     handles.timeVals(1)+handles.timeBoundVals(1)+2,... % Max X
+                     handles.timeVals(1)+handles.timeBoundVals(1)+2,... % Max X
+                     handles.timeVals(1)-handles.timeBoundVals(1)-2];   % Min X
+    
     end % END FOR
     
-    for j = 1:handles.numPatch
+    YY(i,1:4) = [elecPerPatch*(i-1)*10+5,... % Max Y
+                 elecPerPatch*(i-1)*10+5,... % Max Y
+                 elecPerPatch*i*50,...       % Min Y
+                 elecPerPatch*i*50];         % Min Y
+    
+    % For each patch, plot
+    for j = 1:handles.numPatch + 1
         
+        % Plot Patch
         pData = patch(XX(j,:), YY(j,:), 1);
         hold on
         
-        set(pData, 'EdgeColor', 'none')
+        set(pData, 'EdgeColor', 'none'); % Remove edge
+        set(pData, 'FaceAlpha', 0.25); % Set alpha
+        
+        % Change color depending on the patch number
         if ~mod(j,2)
             set(pData, 'FaceColor', 'g')
         else
             set(pData, 'FaceColor', 'b')
         end % END IF
-        set(pData, 'FaceAlpha', 0.1)
-        
     end % END FOR
     hold on
 end % END IF
@@ -391,58 +390,60 @@ end % END IF
 % Plot verticial divisions
 division = 1; % Have a vertical line every 60 seconds
 
+% Calculate the number of vertical lines needed
 numVert = floor((handles.time(end)-handles.time(1))/division) + 4;
 
+% For each vertical line, generate the bounds and plot
 for i = 1:numVert
     hold on
     
-    vertBounds = [(handles.timeVals(1)-handles.timeBoundVals(1)-1) + division*i,...
-                  (handles.timeVals(1)-handles.timeBoundVals(1)-1) + division*i];
+    % Vertical bounds are set for every 60 seconds
+    vertBounds = [(handles.timeVals(1)-handles.timeBoundVals(1)-2) + division*i,...
+                  (handles.timeVals(1)-handles.timeBoundVals(1)-2) + division*i];
     
+    % Plot each vertical line
     plot(vertBounds, [0, numPlot*10+10], 'Color', [0.5, 0.5, 0.5]);
     hold on
 end % END FOR
 
+% Switch plotting based on plotMode
 switch handles.plotMode
-    case 1
-%         plot(handles.time,handles.plotData(handles.chans,:)', 'k');
-        handles.pHandle = plot(handles.time,handles.plotData(:,:)', 'k');
+    case 1 % MonoPhasic
+        handles.pHandle = plot(handles.time,handles.plotData(:,:)', 'k'); % Plot data
         
-        xlim([handles.time(1), handles.time(end)])
+        % Set limits
+        xlim([handles.time(1), handles.time(end)]) 
         ylim([5, handles.numPlotChans*10+5])
         
+        % Set Y tick and label
         set(gca, 'YTick', [10:10:handles.numPlotChans*10])
         set(gca, 'YTickLabel', [handles.plotChans])
         
-        set(zoom(gcf),'Motion','horizontal','Enable','on');
-        set(pan(gcf),'Motion','horizontal','Enable','on');
+    case 2 % BiPhasic
+        handles.pHandle = plot(handles.time,handles.plotData(:,:)', 'k'); % Plot data
         
-    case 2
-        
-        handles.pHandle = plot(handles.time,handles.plotData(:,:)', 'k');
-        
+        % Set limits
         xlim([handles.time(1), handles.time(end)])
         ylim([5, numPlot*10+5])
         
         % Generate Y Labels
         for i = 1:handles.numPlotChans
-            
             labelYAxis{i} = [num2str(handles.plotChans(i)*2-1), '-', num2str(handles.plotChans(i)*2)];
-            
         end % END FOR
         
+        % Set Y tick and label
         set(gca, 'YTick', [10:10:numPlot*10])
         set(gca, 'YTickLabel', labelYAxis)
-        
-        set(zoom(gcf),'Motion','horizontal','Enable','on');
-        set(pan(gcf),'Motion','horizontal','Enable','on');
-        
     otherwise
 end % END SWITCH
 
-zoom(gcf, 10)
+% Enable zoom and pan
+set(zoom(gcf),'Motion','horizontal','Enable','on');
+set(pan(gcf),'Motion','horizontal','Enable','on');
 
-% Set the slide bar
+SetZoomPan(handles);
+
+% Set the slide bar limits and value
 set(handles.TimeSB, 'Max', ((handles.timeVals(1)+handles.timeVals(2)/60) + (handles.timeBoundVals(1)+handles.timeBoundVals(2)/60)));
 set(handles.TimeSB, 'Value', (handles.timeVals(1)+handles.timeVals(2)/60));
 set(handles.TimeSB, 'Min', ((handles.timeVals(1)+handles.timeVals(2)/60) - (handles.timeBoundVals(1)+handles.timeBoundVals(2)/60)));
@@ -453,16 +454,18 @@ guidata(hObject, handles);
 %%%% Load Data %%%% Currently setup for temporary data
 function handles = LoadData(hObject, handles)
 
-tmpChans = max(handles.chans);
+tmpChans = max(handles.chans)+mod(max(handles.chans),2);
 
 dataLen = length([handles.timeCount - handles.timeBoundCount: handles.timeCount + handles.timeBoundCount]);
 
-data = ones(tmpChans, dataLen).*repmat(sind(2*pi*(1:dataLen)), tmpChans,1) + randn(tmpChans, dataLen);
+data = ones(tmpChans, dataLen).*repmat(sin(pi*350*(1:dataLen)./handles.fs), tmpChans,1) + randn(tmpChans, dataLen)/2;
+
+if handles.plotMode == 2
+    data = data([1:2:tmpChans],:) - data([2:2:tmpChans],:);
+end % END IF
 
 scalePos = [1000,100,10,1, 0.1];
-
 scaleVal = scalePos(handles.scale);
-
 handles.data = data.*scaleVal;
 
 guidata(hObject, handles);
@@ -471,67 +474,79 @@ guidata(hObject, handles);
 %%%% Channel Select %%%% Remap the selected channels based on the chosen mode
 function handles = ChanSelect(handles)
 
+% If MonoPhasic was chosen
 if handles.plotMode == 1
     handles.plotChans = handles.chans;
     handles.numPlotChans = length(handles.chans);
-else
+else % If BiPhasic was chosen
     handles.plotChans = handles.chans;
     
+    % Force all odd channels to be even channels
     handles.plotChans(logical(mod(handles.plotChans,2))) = handles.plotChans(logical(mod(handles.plotChans,2)))+1;
     
+    % Gather the selected channels, but neglect repeats
     handles.plotChans = unique(handles.plotChans)/2;
     
+    % Number of plots to plot
     handles.numPlotChans = length(handles.plotChans);
 end % END IF
 % END FUNCTION
 
-%%%% Make Spectrogram %%%%
+%%%% Make Multi-Taper Spectrogram %%%%
 function handles = MakeSpectrogram(handles)
 
-timeSpan = (handles.timeBoundVals(1)*60+handles.timeBoundVals(2))*2;
+% Window size and overlap
+handles.specWindow = [1.5, 0.5];
 
-handles.specWindow = [2, 0.5];
-handles.specparams.tapers = [1,1];
-handles.specParams.Fs = 1000;
-handles.specParams.Fpass = [0,500];
-handles.specParams.pad = 2;
+% MT-Spectrogram params
+handles.specParams.tapers = [3,5];  % Tapers
+handles.specParams.Fs = 1000;       % Fs
+handles.specParams.Fpass = [0,500]; % Fpass
+handles.specParams.pad = 2;         % Padding (next,nextpow2)
 
-[handles.specS,handles.specT,handles.specF]=mtspecgramc(handles.data(handles.specChan,:),handles.specWindow,handles.specParams);
+% Testing Chirp data
+% chirpData = chirp(0:0.001:10*60, 0, 60*10, 500);
 
-figure(2)
-imagesc(handles.specT,handles.specF,10*log10(handles.specS)'); axis xy;
+% Choosing the correct channel (depends on plotMode)
+if handles.plotMode == 1
+    handles.specChanPlot = handles.specChan;
+elseif handles.plotMode == 2
+    handles.specChanPlot = (handles.specChan + mod(handles.specChan,2))/2; % Forces the channel to be even, then divide by 2
+end % END IF
 
-% set(gca, 'YTick', [handles.timeVals(1)-handles.timeBoundVals(1):]
-cbar_axes = colorbar('peer', gca);
-set(get(cbar_axes, 'ylabel'), 'String', 'Power, dB');
-title(sprintf('Channel %d', handles.specChan));
-xlabel('Time, sec')
-ylabel('Frequency, Hz')
+% Calculates the multi-taper spectrogram
+[handles.specS,handles.specT,handles.specF]=mtspecgramc(handles.data(handles.specChanPlot,:),handles.specWindow,handles.specParams);
+% [handles.specS,handles.specT,handles.specF]=mtspecgramc(chirpData,handles.specWindow,handles.specParams);
 
-guidata(hObject, handles);
+figure(2)  % Open a new figure window
+
+% Create a time vector corrosponding to the desired time bound
+specTime = linspace(handles.timeCount - handles.timeBoundCount,...
+                    handles.timeCount + handles.timeBoundCount, length(handles.specT));
+
+% Display the spectrogram
+imagesc(specTime./(handles.fs*60),handles.specF,10*log10(handles.specS)'); 
+axis xy;
+
+cbar_axes = colorbar('peer', gca); % Display colorbar
+set(get(cbar_axes, 'ylabel'), 'String', 'Power, dB'); % Colorbar title
+
+% Display the correct channel information based on plotMode
+if handles.plotMode == 1
+    title(sprintf('Channel %d', handles.specChan)); % Plot Title
+elseif handles.plotMode == 2
+    title(sprintf('Channel %d-%d', handles.specChanPlot*2-1, handles.specChanPlot*2)); % Plot Title
+end % END IF
+
+xlabel('Time, min') % Time label
+ylabel('Frequency, Hz') % Frequency label
+
+% Time offset
+timeOffset = [handles.timeVals(1)-handles.timeBoundVals(1)-1, handles.timeVals(1)+handles.timeBoundVals(1)+1];
+
+% Set tick and tick label
+set(gca, 'XTick', [timeOffset(1):1:timeOffset(2)]);
+set(gca, 'XTickLabel', [timeOffset(1):1:timeOffset(2)]);
 % END FUNCTION
 
 % EOF
-
-
-
-function edit8_Callback(hObject, eventdata, handles)
-% hObject    handle to edit8 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit8 as text
-%        str2double(get(hObject,'String')) returns contents of edit8 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit8_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit8 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
